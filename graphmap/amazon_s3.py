e3e932c152925773f7dc5da6836db0c0ac517275
Abhishek Rao <arao@microsoft.com>
2017-02-04 15:00:59 -0800

    Refactor constants of s3 base

--- graphmap/amazon_s3.py
+++ graphmap/amazon_s3.py
@@ -2,7 +2,6 @@ import os
 import tinys3
 import constants
 
-bucket_name = 'graphnodes'
 key_filename = 'rootkey.csv'
 if os.path.isfile(key_filename):
     with open(key_filename, 'r') as f:
@@ -16,11 +15,11 @@ secret_key = key_file_content[1].strip().split('=')[-1]
 def upload_file(filename, remote_filename):
     conn = tinys3.Connection(access_key_id, secret_key, tls=True)
     if remote_filename.startswith(constants.amazon_s3_base_filename):
-        final_remote_filename = remote_filename[len(constants.amazon_s3_base_filename):]
+        final_remote_filename = constants.amazon_s3_folder + '/'  + remote_filename[len(constants.amazon_s3_base_filename):]
     else:
         final_remote_filename = remote_filename
     with open(filename, 'rb') as f:
-        conn.upload(final_remote_filename, f, bucket_name)
+        conn.upload(final_remote_filename, f, constants.amazon_s3_bucket)
 
 
 if __name__ == '__main__':
